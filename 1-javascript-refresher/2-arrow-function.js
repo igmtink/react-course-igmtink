22d3ff5a89ed9e44b7b66929abcf28f64799d9b9
igmtink <igmtse@gmail.com>
2022-11-30 02:19:58 +0800

    Arrow Function

---
+++ 1-javascript-refresher/2-arrow-function.js
@@ -0,0 +1,13 @@
+// !Normal Function
+function myFunction(myName) {
+  console.log(myName);
+}
+
+myFunction("Ivan");
+
+// !Arrow Function
+const arrowFunction = (myLastName) => {
+  console.log(myLastName);
+};
+
+arrowFunction("Tabalno");
