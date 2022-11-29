07fe29b4fc74204459a7dda63c3fd53598cd523f
igmtink <igmtse@gmail.com>
2022-11-30 02:10:01 +0800

    Let and Const Variable

---
+++ 1-javascript-refresher/1-let-const.js
@@ -0,0 +1,9 @@
+// !Let is a variable that you can re-assign new value
+let myName = "Ivan";
+myName = "Gregor";
+// Output: Gregor
+
+// !Constant is a variable that you can't assign a new value
+const myLastName = "Tabalno";
+myLastName = "Ampler";
+// Output: Error
