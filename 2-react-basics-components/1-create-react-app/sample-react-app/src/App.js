cf0a6daf1ea0a68b0098a3b480c7a09afe7ed3ef
igmtink <igmtse@gmail.com>
2022-11-30 18:48:31 +0800

    Sample React App with TailwindCSS

---
+++ 2-react-basics-components/1-create-react-app/sample-react-app/src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div className="">
+      <h1>Hello world!</h1>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
