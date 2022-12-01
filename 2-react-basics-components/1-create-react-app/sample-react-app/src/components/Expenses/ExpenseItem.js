36b398e157ec4a46c8f8f72bd2681373e7b5f531
igmtink <igmtse@gmail.com>
2022-12-01 16:43:20 +0800

    Organize Components.

---
+++ 2-react-basics-components/1-create-react-app/sample-react-app/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,24 @@
+import Card from "../UI/Card";
+import ExpenseDate from "./ExpenseDate";
+
+// !React Code (JavaScript with HTML)
+const ExpenseItem = (props) => {
+  // !HTML Code
+  return (
+    // !{variable} to pass a (JavaScript) code to (HTML) code
+    // !We can wrapp our (HTML) code inside of Component (Wrapper / Container) because of the special prop that built in ({props.children})
+    // !Also we can set our (className) because of the special prop that built in (props.className)
+    <Card className="bg-zinc-900 py-3 px-5 flex gap-3">
+      {/* We use another (Components / Custom HTML) inside our components, and we pass the (Data) from (Apps.js) to our components (ExpenseItem) and pass it again to another components (ExpenseDate) */}
+      <ExpenseDate date={props.date} time={props.time} />
+      <Card className="bg-zinc-800 p-3 flex flex-1 items-center justify-between">
+        <span>{props.title}</span>
+        <span className="font-medium text-right text-blue-500 w-16">
+          ${props.amount}
+        </span>
+      </Card>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
