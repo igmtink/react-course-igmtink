dfd410d727eae2747009e1cb98d8bec21505fcea
igmtink <igmtse@gmail.com>
2022-12-01 11:05:36 +0800

    Added another Component (ExpenseDate) for date & time.

--- 2-react-basics-components/1-create-react-app/sample-react-app/src/components/ExpenseItem.js
+++ 2-react-basics-components/1-create-react-app/sample-react-app/src/components/ExpenseItem.js
@@ -1,15 +1,13 @@
+import ExpenseDate from "./ExpenseDate";
+
 // !React Code (JavaScript with HTML)
 function ExpenseItem(props) {
   // !HTML Code
   return (
     // !{variable} to pass a (JavaScript) code to (HTML) code
     <div className="bg-zinc-900 py-3 px-5 rounded-md flex gap-3">
-      <div className="grid gap-1">
-        <span className="text-blue-500 font-medium uppercase">
-          {props.date}
-        </span>
-        <span className="text-sm text-zinc-500">{props.time}</span>
-      </div>
+      {/* We use another (Components / Custom HTML) inside our components, and we pass the (Data) from (Apps.js) to our components (ExpenseItem) and pass it again to another components (ExpenseDate) */}
+      <ExpenseDate date={props.date} time={props.time} />
       <div className="bg-zinc-800 rounded-md p-3 flex flex-1 items-center justify-between">
         <span>{props.title}</span>
         <span className="font-medium text-right text-blue-500 w-16">
