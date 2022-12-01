df7d573b7ff1018ca9323e9f7735a507f124650f
igmtink <igmtse@gmail.com>
2022-12-01 19:23:14 +0800

    Event Handler.

---
+++ 3-react-state-events/1-event-handlers/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,31 @@
+import Card from "../UI/Card";
+import ExpenseDate from "./ExpenseDate";
+
+const ExpenseItem = (props) => {
+  // !Function for (onClick) button event handler
+  const clickHandler = () => {
+    document.querySelector("button").textContent = "Clicked!!!";
+  };
+
+  return (
+    <Card className="bg-zinc-900 py-3 px-5 flex gap-3">
+      <ExpenseDate date={props.date} time={props.time} />
+      <Card className="bg-zinc-800 p-3 flex flex-1 items-center justify-between">
+        <span>{props.title}</span>
+        <span className="font-medium text-right text-blue-500 w-16">
+          ${props.amount}
+        </span>
+      </Card>
+
+      {/* !Button (onClick) event handler */}
+      <button
+        className="bg-blue-500 py-1 px-4 rounded-md"
+        onClick={clickHandler}
+      >
+        Click!
+      </button>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
