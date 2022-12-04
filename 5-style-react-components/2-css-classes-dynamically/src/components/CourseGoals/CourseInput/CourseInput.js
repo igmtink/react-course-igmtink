8cd14f9a7842f81af105da379a26294363683ff0
igmtink <igmtse@gmail.com>
2022-12-04 20:26:23 +0800

    Setting CSS Classes Dynamically Styles.

---
+++ 5-style-react-components/2-css-classes-dynamically/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
+    // !We check if the input is not empty
+    if (event.target.value.trim().length !== 0) {
+      setIsValid(true);
+    }
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    // !Before submiting the input, we check if the input is not empty
+    if (enteredValue.trim().length === 0) {
+      // !If the input is empty we set the (isValid) to false
+      setIsValid(false);
+      return;
+    }
+
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      {/* !CSS Classes Dynamically, condition style */}
+      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+        {/* !We check if the (isValid) is not true then we change the color of element into red */}
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
