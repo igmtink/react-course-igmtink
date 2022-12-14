32b4e1d3a555b6306fd3838e5d44690e81777f7f
igmtink <igmtse@gmail.com>
2022-12-14 11:08:10 +0800

    React Memo.

---
+++ 10-bts-react-optimization-techniques/2-react-memo/src/App.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import DemoOutput from "./components/DemoOutput";
+import { Button } from "./components/UI/UIShared";
+
+function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log("APP RUNNING");
+
+  const toggleParagraphHandler = () => {
+    // !This is like (classList.toggle) on DOM
+    // !This is will not activate on the first running the app, because we don't have a (Previous State) in the first place
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
+
+  return (
+    <div className="w-[640px] max-w-full mx-auto p-6 flex justify-center items-center h-screen">
+      <div className="bg-neutral-800 p-4 rounded-md shadow-xl grid grid-cols-1 gap-4 justify-items-center items-center">
+        <div className="grid grid-cols-1 gap-2">
+          <h1 className="text-lg font-bold">Hi there!</h1>
+          <DemoOutput show={false} />
+        </div>
+        <Button attr={{ onClick: toggleParagraphHandler }}>
+          Show Paragraph!
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
