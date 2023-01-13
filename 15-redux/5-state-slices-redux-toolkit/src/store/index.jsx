2510ea153bcf4a5a02b3a9e17b4d06fdcae42917
igmtink <igmtse@gmail.com>
2023-01-13 17:01:18 +0800

    Creating (useContext & useReducer) with (Redux Toolkit).

--- 15-redux/5-state-slices-redux-toolkit/src/store/index.jsx
+++ 15-redux/5-state-slices-redux-toolkit/src/store/index.jsx
@@ -1,11 +1,10 @@
-import { createStore } from 'redux'
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, configureStore } from '@reduxjs/toolkit'
 
 // Initial State
 const initialState = { value: 0, showCounter: true }
 
 // (createSlice) this is like (Reducer)
-createSlice({
+const counterSlice = createSlice({
   name: 'counter',
   initialState,
   // (state) argument on every (reducers method) is the (initialState)
@@ -26,27 +25,7 @@ createSlice({
   }
 })
 
-// !(Reducer) for managing (states)
-const counterReducer = (state = initialState, action) => {
-  // !(Switch Statement) for managing (Dispatch)
-  switch (action.type) {
-    case 'incremented':
-      return { value: state.value + 1, showCounter: state.showCounter }
-    case 'increase':
-      return {
-        value: state.value + action.counter,
-        showCounter: state.showCounter
-      }
-    case 'decremented':
-      return { value: state.value - 1, showCounter: state.showCounter }
-    case 'toggle':
-      return { showCounter: !state.showCounter, value: state.value }
-    default:
-      return state
-  }
-}
-
-// !(createStore) to manage (Reducer) using (Redux)
-const store = createStore(counterReducer)
+// !(configureStore) to manage (createSlice) using (Redux Toolkit) which combines all those (Reducers) methods
+const store = configureStore({ reducer: counterSlice.reducer })
 
 export default store
